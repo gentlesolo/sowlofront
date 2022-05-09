b30d1c0c58883becdd2be659debea0745270b882
Fasina Israel <Fashman28@gmail.com>
2022-05-09 11:12:48 +0100

    register and login updtated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import React from "react";
 import Modal from 'react-modal'
 import BaseRouter from "./baseRouter";
 import Login from './../src/Auth/login';
+import Register from './../src/Auth/Register';
 
 Modal.setAppElement('#root');
 
