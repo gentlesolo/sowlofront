eb9979ed4a946a6cab8012766061ecfd60465112
gentlesolo <solomonarchibong@gmail.com>
2022-05-31 21:47:57 +0100

    Delete src directory

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import contentReducer from '../features/content/contentSlice'
-
-
-export const store = configureStore({
-    reducer: {
-        contents: contentReducer
-    }
-})
